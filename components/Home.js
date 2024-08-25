1917e4d22c202bf45fc174776556b4c6d619728f
kashif <kashifkazmi1412@gmail.com>
2024-08-26 02:01:55 +0500

    Complete Portfolio Frontend

--- components/Home.js
+++ components/Home.js
@@ -39,7 +39,7 @@ const Home = () => {
                 className="hidden xs:block border-4 border-solid border-black-3 mb-25 mx-auto object-cover" style={{height:'160px', width:"160px",borderRadius:"50%"}}
                 src={"assets/img/my-img.jpg"}
                 alt=""
-              />
+              /> 
             {/* Mobile Image Ends */}
             {/* Informations Starts */}
             <h1 className="text-fs-51 text-accent font-Poppins relative uppercase font-bold leading-lh-62 pl-70 before:absolute before:left-0 before:top-29 before:h-4 before:w-40 before:rounded-10 custom-md-2:text-fs-42 down-xl:before:hidden down-xl:pl-0 custom-md-2:leading-lh-52 down-md:text-fs-38 down-md:leading-lh-48 down-md:mt-29 down-md:mb-13 xs:text-fs-29 xs:leading-lh-39 xs:mt-18">
